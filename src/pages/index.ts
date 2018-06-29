f5181448f7877f0dc58e6143701adaa95386ef0a
gufeibai <1294764056@qq.com>
2018-06-29 10:39:43 +0800

    updated data

---
+++ src/pages/index.ts
@@ -0,0 +1,21 @@
+import wepy from 'wepy'
+import { connect } from 'wepy-redux'
+
+export default class Index extends wepy.page {
+  config = {
+    navigationBarTitleText: '...'
+  }
+
+  data = {
+    demo: '待开发'
+  }
+
+  methods = {
+  }
+
+  events = {
+  }
+
+  onLoad() {
+  }
+}
